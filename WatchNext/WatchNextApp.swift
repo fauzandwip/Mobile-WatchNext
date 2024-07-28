ccc3228bfcad0ff922848394726312213ea28856
fauzandwip <fauzan27dp@gmail.com>
2024-07-28 22:53:15 +0700

    Initial Commit

---
+++ WatchNext/WatchNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNextApp.swift
+//  WatchNext
+//
+//  Created by Fauzan Dwi Prasetyo on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
